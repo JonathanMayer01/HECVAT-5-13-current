ef7449a40e070e40bafbb6ea70ea7ccf8ad86492
JonathanMayer01 <jonathanmayer105@gmail.com>
2023-05-13 18:16:45 -0500

    HECVAT 5/13
    login works, page 1 works, client page works

---
+++ HECVAT_Survey/HECVAT_Survery/Pages/Page2(Brett).cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HECVAT_Survey.Pages
+{
+    public class Page2_Brett_Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ HECVAT_Survey/HECVAT_Survery/Pages/Page3(Jennifer).cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HECVAT_Survey.Pages
+{
+    public class Page3_Jennifer_Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
